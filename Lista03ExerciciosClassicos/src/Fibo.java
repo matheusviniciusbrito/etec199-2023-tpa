10e79d8092b9ffc1f398ca6d98bb46ba45138b4f
matheusviniciusbrito <matheus.vinicius.mvrb@gmail.com>
2023-09-01 10:58:37 -0300

    Exercicios de TPA resolvidos até o dia 01/09/2023

---
+++ Lista03ExerciciosClassicos/src/Fibo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Fibo {
+	public static void main(String[] args) {
+		int i=1, anterior=0, atual=1, proximo, n;
+		Scanner in = new Scanner(System.in);
+		System.out.println("entre com o n ");
+		n = in.nextInt();
+	}
+
+}
