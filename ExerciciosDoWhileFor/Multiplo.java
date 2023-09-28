c5dc7da5e21952995ba1b49777e87c2475d926e3
Matheus Vinicius <78880370+matheusviniciusbrito@users.noreply.github.com>
2023-09-28 18:47:04 -0300

    Update Multiplo.java

--- ExerciciosDoWhileFor/Multiplo.java
+++ ExerciciosDoWhileFor/Multiplo.java
@@ -3,9 +3,8 @@ public class Multiplo {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in); {
 		
-		int i;	
 		
-		for (i=1; i<=100; i++) {
+		for (int i=1; i<=100; i++) {
 			if(i % 10 == 0) {
 				System.out.println(i+" Multiplo de 10");
 				
